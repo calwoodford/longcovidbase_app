20821ae3afe7830b705c983e43ce06ad9c649880
Cal Woodford <callum@woodford.info>
2023-10-20 12:58:34 +0100

    added: eye.png, description text

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, //removes the debug banner
 
       home: Scaffold(
-        backgroundColor: Color(0xFF07122A),
+        backgroundColor: const Color(0xFF07122A),
         body: SafeArea(
           child: SingleChildScrollView(
             child: Center(
@@ -26,29 +26,29 @@ class MyApp extends StatelessWidget {
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      SizedBox(width: 200.0, height: 140.0),
+                      const SizedBox(width: 200.0, height: 140.0),
                       Image.asset(
                         'assets/images/transparentlogogif.gif',
                         height: 200,
                       ),
-                      SizedBox(width: 200.0, height: 50.0),
+                      const SizedBox(width: 200.0, height: 50.0),
                       Text(
                         'Welcome to Long Covid Base',
                         style: GoogleFonts.montserrat(
-                          textStyle: TextStyle(
+                          textStyle: const TextStyle(
                               fontSize: 14, fontWeight: FontWeight.bold),
                           color: Colors.white,
                         ),
                       ),
-                      SizedBox(width: 200.0, height: 30.0),
+                      const SizedBox(width: 200.0, height: 30.0),
                       Text(
                         'longcovidbase.com',
                         style: GoogleFonts.montserrat(
-                          textStyle: TextStyle(fontSize: 10),
+                          textStyle: const TextStyle(fontSize: 10),
                           color: Colors.white,
                         ),
                       ),
-                      SizedBox(width: 200.0, height: 60.0),
+                      const SizedBox(width: 200.0, height: 60.0),
                       Container(
                         width: 245,
                       ),
@@ -68,7 +68,7 @@ class MyWidget extends StatefulWidget {
   final String labelText;
   final IconData? suffixIcon; // New property for suffix icon
 
-  MyWidget({required this.labelText, this.suffixIcon});
+  const MyWidget({super.key, required this.labelText, this.suffixIcon});
 
   @override
   _MyWidgetState createState() => _MyWidgetState();
@@ -81,7 +81,7 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(
+        SizedBox(
           height: 30,
           child: TextField(
             onChanged: (text) {
@@ -89,13 +89,13 @@ class _MyWidgetState extends State<MyWidget> {
                 _inputText = text;
               });
             },
-            style: TextStyle(fontSize: 12),
+            style: const TextStyle(fontSize: 12),
             decoration: InputDecoration(
               suffixIcon: widget.suffixIcon != null
                   ? Icon(widget.suffixIcon,
                       size: 20) // Use the provided suffix icon
                   : null, // If no suffix icon is provided, set to null
-              contentPadding: EdgeInsets.symmetric(
+              contentPadding: const EdgeInsets.symmetric(
                 vertical: 1, // Adjust the vertical padding
                 horizontal: 1,
               ), // Adjust the horizontal padding
